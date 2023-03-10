0da2e034f27469c8fd7e6ce7e2f820c09f79e725
danxsodev <danilloxsoliveira13@gmail.com>
2023-03-10 15:35:11 -0300

    Initial commit

---
+++ alocacao/alocacao/src/test/java/br/gov/sp/etec/alocacao/model/CarTest.java
@@ -0,0 +1,16 @@
+package br.gov.sp.etec.alocacao.model;
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		Car color = new Car();
+		
+		color.setColor("amarelo");
+		System.out.println(color.getColor());
+		
+		
+		
+		
+	}
+
+}
