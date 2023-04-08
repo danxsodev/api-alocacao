d4a4f585c03e33baa5b6a785d91cdbaa0b3f52b3
Danillo Xavier <danilloxsoliveira13@gmail.com>
2023-04-08 14:36:20 -0300

    refactor model

--- src/main/java/br/sp/gov/etec/alocacao/model/Car.java
+++ src/main/java/br/sp/gov/etec/alocacao/model/Car.java
@@ -1,7 +1,17 @@
 package br.sp.gov.etec.alocacao.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
 public class Car {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
 	private Long id;
 	private String model;
 	private String color;
