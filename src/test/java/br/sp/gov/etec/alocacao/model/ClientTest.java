0bb6fd7c92948230bc0f799b0699b167ffb3c500
Danillo Xavier <danilloxsoliveira13@gmail.com>
2023-04-07 22:11:09 -0300

    creating test on models

---
+++ src/test/java/br/sp/gov/etec/alocacao/model/ClientTest.java
@@ -0,0 +1,13 @@
+package br.sp.gov.etec.alocacao.model;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+		Client name = new Client();
+		
+		name.setName("Lucas");
+		System.out.println(name.getName());
+
+	}
+
+}
