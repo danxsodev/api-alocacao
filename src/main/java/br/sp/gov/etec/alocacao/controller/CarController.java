b24d5ffe1cd14096dedc0533f4cac74a8227e601
Danillo Xavier <danilloxsoliveira13@gmail.com>
2023-04-08 14:56:31 -0300

    refactor controller

--- src/main/java/br/sp/gov/etec/alocacao/controller/CarController.java
+++ src/main/java/br/sp/gov/etec/alocacao/controller/CarController.java
@@ -3,35 +3,32 @@ package br.sp.gov.etec.alocacao.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.sp.gov.etec.alocacao.model.Car;
+import br.sp.gov.etec.alocacao.repository.CarRepository;
+import jakarta.security.auth.message.callback.PrivateKeyCallback.Request;
 
 @RestController
 public class CarController {
 	
+	@SuppressWarnings("unchecked")
+	
+	@Autowired
+	CarRepository repository;
+	
 	@GetMapping("listar-carros")
 	public List<Car> listCars(){
-		Car car1 = new Car();
-		car1.setId(1L);
-		car1.setColor("amarelo");
-		car1.setModel("HB20");
-		car1.setLicensePlate("ABC-1234");
-		car1.setChassi(123455666790L);
-		
-		Car car2 = new Car();
-		car2.setId(2L);
-		car2.setColor("vermelho");
-		car2.setModel("Corsa");
-		car2.setLicensePlate("CBA-1234");
-		car2.setChassi(123455666710L);
-		
-		List cars = new ArrayList<Car> ();
-		cars.add(car1);
-		cars.add(car2);
-		return cars;
+		return repository.findAll();
+	}
+	
+	@PostMapping("adicionar-carro")
+	public Car insertCars(@RequestBody Car car) {
+		return repository.save(car);
 	}
 	
 }
--- src/main/java/br/sp/gov/etec/alocacao/controller/ClientController.java
+++ src/main/java/br/sp/gov/etec/alocacao/controller/ClientController.java
@@ -3,37 +3,34 @@ package br.sp.gov.etec.alocacao.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.sp.gov.etec.alocacao.model.Car;
 import br.sp.gov.etec.alocacao.model.Client;
+import br.sp.gov.etec.alocacao.repository.CarRepository;
+import br.sp.gov.etec.alocacao.repository.ClientRepository;
+import jakarta.security.auth.message.callback.PrivateKeyCallback.Request;
 
 @RestController
 public class ClientController {
 	
+	@SuppressWarnings("unchecked")
+	
+	@Autowired
+	ClientRepository repository;
+	
 	@GetMapping("listar-clientes")
 	public List<Client> listClient() {
-		Client client1 = new Client();
-		client1.setId(1L);
-		client1.setName("Andreia");
-		client1.setCpf("404.612.100-96");
-		client1.setPhone("(13)3825-4065");
-		client1.setMail("andreia731@gmail.com");
-		
-		
-		Client client2 = new Client();
-		client2.setId(2L);
-		client2.setName("Lucas");
-		client2.setCpf("404.632.150-54");
-		client2.setPhone("(11)3925-5342");
-		client2.setMail("lucas181@gmail.com");
-		
-		List clients = new ArrayList<Client> ();
-		clients.add(client1);
-		clients.add(client2);
-		return clients;
+		return repository.findAll();
 	}
+	
+    @PostMapping("adicionar-cliente")
+    public Client insertClient(@RequestBody Client client) {
+    	return repository.save(client);
+    }
 	
 }
 	
