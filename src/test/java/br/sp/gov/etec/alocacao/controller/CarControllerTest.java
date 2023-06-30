63deef476b3383c8052a77cca16b2be7b88dd69e
Danillo Xavier <danilloxsoliveira13@gmail.com>
2023-06-30 18:55:59 -0300

    test: Controllers Car and Client

--- src/test/java/br/sp/gov/etec/alocacao/controller/CarControllerTest.java
+++ src/test/java/br/sp/gov/etec/alocacao/controller/CarControllerTest.java
@@ -1,14 +1,23 @@
 package br.sp.gov.etec.alocacao.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import br.sp.gov.etec.alocacao.controller.CarController;
+
+import br.sp.gov.etec.alocacao.repository.CarRepository;
 import br.sp.gov.etec.alocacao.model.Car;
 
 @ExtendWith(SpringExtension.class)
@@ -17,12 +26,62 @@ public class CarControllerTest {
 	@InjectMocks
 	CarController controller;
 	
+	@Mock
+	CarRepository repository;
+	
+	@Mock 
+	List<Car> listCars;
+	
+	@Mock
+	Car car;
+	
 	@Test
-	public void listCarsTest() {
-		List <Car> cars = controller.listCars();
+	public void testListCars() {
+		when(repository.findAll()).thenReturn(listCars);
+		 List<Car> cars = controller.listCars();
+		 assertNotNull(cars);
+	}
+	
+	@Test
+	public void testeNull() {
+		when(repository.findAll()).thenReturn(null);
+		 List<Car> cars = controller.listCars();
+		 assertNull(cars);
+	}
+	
+	@Test
+	public void testAddCarSucess() {
+		when(repository.save(any(Car.class))).thenReturn(getCar());
+		Car car = controller.updateCar(new Car("Fusca","Branco", "ABC123", 1234L));
+		assertEquals("Fusca", car.getModel());
+		assertEquals("Branco", car.getColor());
+		assertEquals("ABC123", car.getLicensePlate());
+		assertEquals(1234L, car.getChassi());
+		assertEquals(1L, car.getId());
 		
-		assertNotNull(cars);
-		
+	}
+	
+	@Test
+	public void testUpdateCarSucess() {
+		when(repository.save(any(Car.class))).thenReturn(getCar());
+		Car car = controller.updateCar(getCar());
+		assertEquals("Fusca", car.getModel());
+	
+	}
+	
+	@Test
+	public void testDeleteCarSucess() {
+		controller.deleteCar(1L);
+	}
+	
+	private Car getCar() {
+		Car car = new Car();
+		car.setId(1L);
+		car.setModel("Fusca");
+		car.setColor("Branco");
+		car.setLicensePlate("ABC123");
+		car.setChassi(1234L);
+		return car;
 	}
 	
 }
--- src/test/java/br/sp/gov/etec/alocacao/controller/ClientControllerTest.java
+++ src/test/java/br/sp/gov/etec/alocacao/controller/ClientControllerTest.java
@@ -1,15 +1,22 @@
 package br.sp.gov.etec.alocacao.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import br.sp.gov.etec.alocacao.controller.ClientController;
+
 import br.sp.gov.etec.alocacao.model.Client;
+import br.sp.gov.etec.alocacao.repository.ClientRepository;
 
 @ExtendWith(SpringExtension.class)
 public class ClientControllerTest {
@@ -17,12 +24,28 @@ public class ClientControllerTest {
 	@InjectMocks
 	ClientController controller;
 	
+	@Mock
+	ClientRepository repository;
+	
 	@Test
-	public void listClientTest() {
-		List <Client> clients = controller.listClient();
+	public void testClients() {
+		when(repository.findAll()).thenReturn(getListClient());
+		List<Client> client = controller.listClient();
+		assertNotNull(client);
+		assertEquals("Francisco", client.get(0).getName());
+	}
+	
+	private ArrayList<Client> getListClient(){
+		Client client = new Client();
+		client.setName("Francisco");
+		client.setCpf("123.321.213-31");
+		client.setMail("francisco@gmail.com");
+		client.setPhone("(11)96738-2612");
+		client.setId(1L);
 		
-		assertNotNull(clients);
-		
+		ArrayList<Client> list = new ArrayList<Client>();
+		list.add(client);
+		return list;
 	}
 	
 }
